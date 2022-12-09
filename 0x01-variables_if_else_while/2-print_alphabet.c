4d0ba85f6e24c43d47d2d7fc3729f0bc6f3f2f2a
mourine wangu <mourinenjeri2020@gmail.com>
2022-12-09 10:38:10 -0800

    task two

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-char lc;
-for
+	char lc;
+	for
 (lc = 'a'; lc <= 'z'; lc++);
-{
-putchar(lc);
-}
-putchar('\n');
-return (0);
+	{
+	putchar(lc);
+	}
+	putchar('\n');
+	return (0);
 }
