2c461ae05725a85515179108d330ceec6c22e51a
mourine wangu <mourinenjeri2020@gmail.com>
2022-12-09 06:57:22 -0800

    task three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints an alphabet in both lower and upper case
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+for (ch = 'A'; ch <= 'z'; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
