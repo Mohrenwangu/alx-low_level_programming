65bc228e4dc82e7e632e4d5102433cfca8b1de3e
mourine wangu <mourinenjeri2020@gmail.com>
2022-12-09 07:30:35 -0800

    task three

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,15 @@
  */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
-putchar(ch);
-for (ch = 'A'; ch <= 'z'; ch++)
-putchar(ch);
-putchar('\n');
-return (0);
+
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	putchar(ch);
+
+	for (ch = 'A'; ch <= 'z'; ch++)
+	putchar(ch);
+
+	putchar('\n');
+	return (0);
 }
